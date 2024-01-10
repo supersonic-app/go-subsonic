8df30080ab8d6bbc0a8049ce0578272b076a85eb
Drew Weymouth <dweymouth@gmail.com>
2024-01-09 19:03:14 -0800

    openSubsonic update

--- subsonic/opensubsonic.go
+++ subsonic/opensubsonic.go
@@ -2,10 +2,11 @@ package subsonic
 
 const (
 	SongLyricsExtension = "songLyrics"
+	TranscodeOffset     = "transcodeOffset"
 )
 
 func (c *Client) GetOpenSubsonicExtensions() ([]*OpenSubsonicExtension, error) {
-	resp, err := c.Get("getPlaylists", nil)
+	resp, err := c.Get("getOpenSubsonicExtensions", nil)
 	if err != nil {
 		return nil, err
 	}
@@ -15,6 +16,9 @@ func (c *Client) GetOpenSubsonicExtensions() ([]*OpenSubsonicExtension, error) {
 	return resp.OpenSubsonicExtensions.OpenSubsonicExtensions, nil
 }
 
+// Get structured lyrics for a track.
+//
+// Server must support OpenSubsonic songLyrics extension
 func (c *Client) GetLyricsBySongId(songID string) (*LyricsList, error) {
 	resp, err := c.Get("getLyricsBySongId", map[string]string{"id": songID})
 	if err != nil {
